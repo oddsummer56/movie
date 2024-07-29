1f938675ff75ab79a8a95b85c14049fed57dee87
oddsummer56 <oddsummer56@gmail.com>
2024-07-29 15:28:23 +0900

    1 deploy

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -2,8 +2,8 @@ import requests
 import os
 import pandas as pd
 
-def save2df():
-    df=list2df()
+def save2df(load_dt='20120101'):
+    df=list2df(load_dt)
     # df에 load_dt 칼럼 추가 (조회 일자 YYYYMMDD 형식)
     # 아래 파일 저장시 load_dt 기준으로 파티셔닝
     df['load_dt'] = '20120101'
@@ -11,13 +11,13 @@ def save2df():
     df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
     return df
     
-def list2df():
-    l = req2list()
+def list2df(load_dt='20120101'):
+    l = req2list(load_dt)
     df = pd.DataFrame(l)
     return df
 
-def req2list() -> list:
-    _, data = req()
+def req2list(load_dt='20120101') -> list:
+    _, data = req(load_dt)
     l = data['boxOfficeResult']['dailyBoxOfficeList']
     return l
 
@@ -25,9 +25,9 @@ def get_key():
     key = os.getenv('MOVIE_API_KEY')
     return key
 
-def req(dt="20120101"):
+def req(load_dt="20120101"):
     #url = gen_url('20240720')
-    url = gen_url(dt)
+    url = gen_url(load_dt)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
