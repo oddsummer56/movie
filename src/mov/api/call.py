4f65b12b8d482b40bc49a3bb61d8d4e1ed2e3882
oddsummer56 <oddsummer56@gmail.com>
2024-07-29 11:36:27 +0900

    save2df

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -1,25 +1,43 @@
-import requests 
+import requests
 import os
+import pandas as pd
+
+def save2df():
+    df=list2df()
+    # df에 load_dt 칼럼 추가 (조회 일자 YYYYMMDD 형식)
+    # 아래 파일 저장시 load_dt 기준으로 파티셔닝
+    df['load_dt'] = '20120101'
+    print(df.head(5))
+    df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
+    return df
+    
+def list2df():
+    l = req2list()
+    df = pd.DataFrame(l)
+    return df
+
+def req2list() -> list:
+    _, data = req()
+    l = data['boxOfficeResult']['dailyBoxOfficeList']
+    return l
+
+def get_key():
+    key = os.getenv('MOVIE_API_KEY')
+    return key
 
 def req(dt="20120101"):
+    #url = gen_url('20240720')
     url = gen_url(dt)
     r = requests.get(url)
     code = r.status_code
     data = r.json()
     print(data)
     return code, data
-    
+
+
 def gen_url(dt="20120101"):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = get_key()
     url = f"{base_url}?key={key}&targetDt={dt}"
+
     return url
-
-def get_key():
-    key = os.getenv('MOVIE_API_KEY')     
-    return key
-
-def req2dataframe():
-    _, data = req()
-    l = data['boxOfficeResult']['dailyBoxOfficeList']
-    return l
--- tests/test_call.py
+++ tests/test_call.py
@@ -1,12 +1,34 @@
-from mov.api.call import gen_url, req, get_key, req2dataframe
+from mov.api.call import gen_url, req, get_key, req2list, list2df, save2df
+import pandas as pd
 
-def test_get_key():
+def test_save2df():
+    df = save2df()
+    assert isinstance(df, pd.DataFrame)
+    assert 'load_dt' in df.columns
+    
+def test_list2df():
+    df = list2df()
+    print(df)
+    assert isinstance(df, pd.DataFrame)
+    assert 'rnum' in df.columns
+    assert 'openDt' in df.columns
+    assert 'movieNm' in df.columns
+    assert 'audiAcc' in df.columns
+
+def test_req2list():
+    l = req2list()
+    assert len(l) > 0
+    v = l[0]
+    assert 'rnum' in v.keys()
+    assert v['rnum'] == '1'
+
+def test_비밀키숨기기():
     key = get_key()
     assert key
-    
-def test_gen_url():
+
+
+def test_유알엘테스트():
     url = gen_url()
-    assert True
     assert "http" in url
     assert "kobis" in url
 
@@ -14,9 +36,6 @@ def test_req():
     code, data = req()
     assert code == 200
 
-def test_req2dataframe():
-    l = req2dataframe()
-    assert len(l)>0
-    v=l[0]
-    assert 'rnum' in v.keys()
-    assert v['rnum'] == '1'
+    code, data = req('20230101')
+    assert code == 200
+
