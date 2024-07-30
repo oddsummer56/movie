390674851dff2eb2ad164ac5aa994edcbdc64267
oddsummer56 <oddsummer56@gmail.com>
2024-07-30 15:52:00 +0900

    merge fix

--- tests/test_call.py
+++ tests/test_call.py
@@ -1,5 +1,8 @@
-from mov.api.call import gen_url, req, get_key, req2list, list2df, save2df
+from mov.api.call import gen_url, req, get_key, req2list, list2df, save2df, echo
 import pandas as pd
+
+def test_echo():
+    assert False
 
 def test_save2df():
     df = save2df()
